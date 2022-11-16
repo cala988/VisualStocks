51667ab834fd8ec094567a65e7844b51c457145a
clafuente <clafuente@atsistemas.com>
2022-11-16 15:22:23 +0100

    Modify interface

--- VisualStocks.Application/Interfaces/IScrapperService.cs
+++ VisualStocks.Application/Interfaces/IScrapperService.cs
@@ -4,7 +4,9 @@ namespace VisualStocks.Application.Interfaces
 {
     public interface IScrapperService
     {
+        string GetAttributeOfXPath(HtmlDocument doc, string xPathDemand, string attribute);
+        void GetCellOfTables(HtmlDocument doc, string tableXpath);
         public HtmlDocument GetDocument(string url);
-
+        string GetInnerTextOfXPath(HtmlDocument doc, string xPathDemand);
     }
 }
