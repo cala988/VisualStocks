ade655327d531bc5f44935e6361e9e31f674f601
clafuente <clafuente@atsistemas.com>
2022-11-18 07:40:53 +0100

    [REFACTOR] Create extension for HtmlDocuments

---
+++ VisualStocks.Application/Extensions/HtmlDocumentExtension.cs
@@ -0,0 +1,32 @@
+﻿using HtmlAgilityPack;
+
+namespace VisualStocks.Application.Extensions
+{
+    public static class HtmlDocumentExtension
+    {
+        public static string GetInnerTextOfXPath(this HtmlDocument doc, string xPathDemand)
+        {
+            return doc.DocumentNode
+                    .SelectNodes(xPathDemand)
+                    .First()
+                    .GetDirectInnerText();
+        }
+
+        public static string GetAttributeOfXPath(this HtmlDocument doc, string xPathDemand, string attribute)
+        {
+            return doc.DocumentNode
+                .SelectNodes(xPathDemand)
+                .First()
+                .Attributes[attribute].Value;
+        }
+
+        public static IEnumerable<object> GetCellOfTables(this HtmlDocument doc, string tableXpath)
+        {
+            return from table in doc.DocumentNode.SelectNodes(tableXpath).Cast<HtmlNode>()
+                        from row in table.SelectNodes("tr").Cast<HtmlNode>()
+                        from cell in row.SelectNodes("th|td").Cast<HtmlNode>()
+                        select new { Table = table.Id, CellText = cell.InnerText };
+        }
+
+    }
+}
--- VisualStocks.Infrastructure/Services/ScrapperService.cs
+++ VisualStocks.Infrastructure/Services/ScrapperService.cs
@@ -12,36 +12,6 @@ namespace VisualStocks.Infrastructure.Services
             HtmlDocument doc = web.Load(url);
             return doc;
         }
-
-        public string GetInnerTextOfXPath(HtmlDocument doc, string xPathDemand)
-        {
-            return doc.DocumentNode
-                    .SelectNodes(xPathDemand)
-                    .First()
-                    .GetDirectInnerText();
-        }
-
-        public string GetAttributeOfXPath(HtmlDocument doc, string xPathDemand, string attribute)
-        {
-            return doc.DocumentNode
-                .SelectNodes(xPathDemand)
-                .First()
-                .Attributes[attribute].Value;
-        }
-
-        public void GetCellOfTables(HtmlDocument doc, string tableXpath)
-        {
-            var query = from table in doc.DocumentNode.SelectNodes(tableXpath).Cast<HtmlNode>()
-                        from row in table.SelectNodes("tr").Cast<HtmlNode>()
-                        from cell in row.SelectNodes("th|td").Cast<HtmlNode>()
-                        select new { Table = table.Id, CellText = cell.InnerText };
-
-            foreach (var cell in query)
-            {
-                Console.WriteLine("{0}: {1}", cell.Table, cell.CellText);
-            }
-
-        }
         
     }
 }
