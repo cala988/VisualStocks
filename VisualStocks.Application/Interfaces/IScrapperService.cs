814c88627e42e6226325e97afe39d477b76aa070
clafuente <clafuente@atsistemas.com>
2022-11-18 07:42:12 +0100

    Refactor obsolete methods

--- VisualStocks.Application/Interfaces/IScrapperService.cs
+++ VisualStocks.Application/Interfaces/IScrapperService.cs
@@ -4,9 +4,6 @@ namespace VisualStocks.Application.Interfaces
 {
     public interface IScrapperService
     {
-        string GetAttributeOfXPath(HtmlDocument doc, string xPathDemand, string attribute);
-        void GetCellOfTables(HtmlDocument doc, string tableXpath);
         public HtmlDocument GetDocument(string url);
-        string GetInnerTextOfXPath(HtmlDocument doc, string xPathDemand);
     }
 }
