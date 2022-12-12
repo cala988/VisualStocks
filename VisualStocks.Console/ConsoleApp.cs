3e677e247878f298343b347420918fbeb6cd4128
clafuente <clafuente@atsistemas.com>
2022-12-12 16:23:48 +0100

    Add console project

---
+++ VisualStocks.Console/ConsoleApp.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using VisualStocks.Application.Interfaces;
+
+namespace VisualStocks.Console
+{
+    public class ConsoleApp : IHostedService
+    {
+        private readonly IStocksService _stocksService;
+
+        public ConsoleApp(IStocksService stocksService)
+        {
+            _stocksService = stocksService;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                _stocksService.GetStock("QCOM");
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex);
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ VisualStocks.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using VisualStocks.Console;
+
+IHost host = Host
+    .CreateDefaultBuilder(args)
+
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<IHostedService, ConsoleApp>();
+    })
+    .Build();
+
+host.RunAsync();
