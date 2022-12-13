a06f60937ef32d03019e047d39529ab939248128
clafuente <clafuente@atsistemas.com>
2022-12-13 16:21:14 +0100

    Add stock prediction calculator

---
+++ VisualStocks.Application/Interfaces/IStockPredictionCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace VisualStocks.Application.Interfaces
+{
+    public interface IStockPredictionCalculator
+    {
+        public double GetFuturePrice(int years, double EpsGrowth5Years, double price);
+
+        public double GetFutureDividend(int years, double DividendGrowth5Years, double dividend);
+
+        public double GetFuturePriceInBaseOfAverageSectorPE(double futurePrice, double AverageSectorPE, double actualStockPE);
+        
+        public double GetFuturePriceInBaseOfAverageStockPE(double futurePrice, double averageStockPE, double actualStockPE);
+
+        public double GetFutureEPS(int years, double EpsGrowth5Years, double eps);
+    }
+}
---
+++ VisualStocks.Application/Services/StockPredictionCalculator.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Diagnostics;
+using VisualStocks.Application.Helpers;
+using VisualStocks.Application.Interfaces;
+
+namespace VisualStocks.Application.Services
+{
+    public class StockPredictionCalculator : IStockPredictionCalculator
+    {
+        public double GetFutureDividend(int years, double DividendGrowth5Years, double dividend)
+        {
+            return MathCalculator.GetCompoundInterest(years, DividendGrowth5Years, dividend);
+        }
+
+        public double GetFutureEPS(int years, double EpsGrowth5Years, double eps)
+        {
+            return MathCalculator.GetCompoundInterest(years, EpsGrowth5Years, eps);
+        }
+
+        public double GetFuturePrice(int years, double EpsGrowth5Years, double price)
+        {
+            return MathCalculator.GetCompoundInterest(years, EpsGrowth5Years, price);
+        }
+
+        public double GetFuturePriceInBaseOfAverageStockPE(double futurePrice, double averageStockPE, double actualStockPE)
+        {
+            return futurePrice * averageStockPE / actualStockPE;
+        }
+
+        public double GetFuturePriceInBaseOfAverageSectorPE(double futurePrice, double AverageSectorPE, double actualStockPE)
+        {
+            return futurePrice * AverageSectorPE / actualStockPE;
+        }
+    }
+}
