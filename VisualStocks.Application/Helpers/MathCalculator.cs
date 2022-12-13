56ea028e5e6a99487ddda4e1ff814dc0dacce9a8
clafuente <clafuente@atsistemas.com>
2022-12-13 16:19:56 +0100

    Add math calculator helper

---
+++ VisualStocks.Application/Helpers/MathCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace VisualStocks.Application.Helpers
+{
+    public static class MathCalculator
+    {
+        public static double GetCompoundInterest(int time, double interest, double initialValue)
+        {
+            double interestPercentageCalculation = (interest / 100) + 1;
+            double hightInterestYear = Math.Pow(interestPercentageCalculation, time);
+            double finalValue = initialValue * hightInterestYear;
+
+            return finalValue;
+        }
+    }
+}
